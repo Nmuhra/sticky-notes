d409ef4c169e9058f532ab94f6ae248b5164630f
Nabegh <97016646+Nmuhra@users.noreply.github.com>
2022-03-16 18:46:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from tkinter.filedialog import*
 import tkinter as tk
 
+note_color = input("Note color: ")
 canvas = tk.Tk()
 canvas.geometry("400x400")
 canvas.title("Notes")
-canvas.config(bg = "yellow")
+canvas.config(bg = str(note_color))
 top = Frame(canvas)
 top.pack(padx = 10, pady = 5, anchor = "nw")
 
@@ -20,6 +21,7 @@ b3.pack(in_ = top, side=LEFT)
 b4 = Button(canvas, text="exit", bg = "lightblue")
 b4.pack(in_ = top, side=LEFT)
 
-entry = Text(canvas, bg = "green", font = ("arial", 12))
+paragraph_color = input("paragraph color: ")
+entry = Text(canvas, bg = str(paragraph_color), font = ("arial", 12))
 entry.pack(padx = 10,pady = 10)
 canvas.mainloop()
